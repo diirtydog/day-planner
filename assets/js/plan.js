9f16f63b52779f3d8025dcd4d076c4d1c3e4829f
andrew wales <andrew.derek.wales@gmail.com>
2021-12-10 16:16:08 -0600

    trying to add get item

--- assets/js/plan.js
+++ assets/js/plan.js
@@ -1,23 +1,10 @@
+// add queryselector for the button click
+var buttonEl = document.querySelector("#save-me");
+
 // create an open array so the user text has a place to enter
-var tasks = {};
+var tasks = [];
 
-
-//<div class="row col-12">
-        //<h3 class="time-block col-2 hour">9AM</h3>
-        //<span class="textarea col-8">Enter Activities</span>
-        //<button class="saveBtn col-2 bi bi-save"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
-          //<path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
-        //</svg></button>
-      //</div>
-// generate html blocks for each hour of the day
-var createTask = function(tasks) {
-    var taskDiv = $("<div>").addClass("row col-12");
-    var taskH3 = $('<h3>').addClass('time-block col-2 hour').text("9AM");
-    var taskSpan = $('<span>').addClass('textarea col-8').text('Enter Activities');
-    var taskBtn = $('<button>').addClass('saveBtn col-2').text();
-    $('container').append(taskDiv, taskH3, taskSpan, taskBtn);
-}
-
+// add the ability to edit these blocks
 $(".container").on("click", "span", function() {
     var text = $(this)
     .text()
@@ -28,12 +15,78 @@ $(".container").on("click", "span", function() {
     $(this).replaceWith(textInput);
       textInput.trigger("focus");
     console.log(this);
-  });
+    tasks.push(textInput)
+    saveTasks();
+});
 
-createTask();
-console.log(createTask)
-// add the ability to edit these blocks
+$("#save-me").click(function() {
+    var taskText = $("<textarea>").val();
+
+    if (taskText) {
+        loadTasks;
+
+    }
+})
 
 // add the ability to save these blocks in the local storage
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
-// make the blocks change color according to past present and future
+buttonEl.addEventListener("click", saveTasks);
+
+ $(".container").on("click", "textarea", function() {
+    // get the textarea's current value/text
+    var text = $(this)
+      .val()
+      .trim();
+  
+    // get the parent's id attribute
+    var status = $(this)
+      .closest(".container")
+      .attr("id")
+      .replace("text-1", "");
+  
+    // get the task's position in the list of other div elements
+    var index = $(this)
+      .closest(".container")
+      .index();
+    tasks[status][index].text = text;
+    saveTasks();
+  
+    // recreate p element
+    var taskP = $("<span>")
+      .addClass("textarea col-8")
+      .text(text);
+  
+    // replace textarea with p element
+    $(this).replaceWith(taskP);
+    console.log('click');
+});
+  
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+  
+    if (!savedTasks) {
+      tasks = [];
+      return false;
+    }
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    savedTasks = JSON.parse(savedTasks);
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!tasks) {
+      tasks = {};
+    }
+    
+    // loop over object properties
+    $.each(tasks, function(list, arr) {
+       //then loop over sub-array
+        arr.forEach(function(tasks) {
+            createTask(tasks.text, list);
+        });
+    });
+};
+loadTasks();
+// saveTasks();
+// console.log(tasks)
+// // make the blocks change color according to past present and future
