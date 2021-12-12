7f76fed33b5528b42d526807c7ac6f5d7e931e3a
diirtydog <andrew.derek.wales@gmail.com>
2021-12-12 17:37:36 -0600

    finally got some saturation on the rows lol

--- assets/js/plan.js
+++ assets/js/plan.js
@@ -1,8 +1,29 @@
-// add queryselector for the button click
-//var buttonEl = document.querySelector("#save-me");
-
-// create an open array so the user text has a place to enter
+// get todays date and make hour variable
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+var timeCurrent = moment().format("HH")-6;
 var tasks = [];
+// call the timer function so it loads instantly on the page
+timer();
+function timer() {
+  // loop over rows
+  $(".row").each(function() {
+      var setTime = parseInt($(this).attr('id'));
+      timeCurrent = parseInt(timeCurrent)
+      // To check the time and add the classes for background indicators
+      if (timeCurrent == setTime) {
+        $(this).addClass("present");
+        $(this).children(".textarea").addClass("white-text");
+      } else if (timeCurrent < setTime) {
+        $(this).removeClass("present");
+        $(this).children(".textarea").addClass("future");
+      } else if (timeCurrent > setTime) {
+        $(this).removeClass("future");
+        $(this).children(".textarea").addClass("past");
+      }
+      console.log(setTime, timeCurrent);
+  })
+};
 
 // add the ability to edit these blocks
 $(".row").on("click", "textarea", function() {
@@ -16,9 +37,9 @@ $(".row").on("click", "textarea", function() {
       textInput.trigger("focus");
     console.log(this);
     tasks.push(textInput)
-    //saveTasks();
 });
 
+// save items 
 $(".saveBtn").on('click', function() {
     var userInput = $(this)
     .siblings(".textarea")
@@ -26,77 +47,22 @@ $(".saveBtn").on('click', function() {
     var hourSpan = $(this)
     .parent()
     .attr('id')
-    localStorage.setItem(hourSpan, JSON.stringify(userInput));
-
+    localStorage.setItem(hourSpan, userInput);
 });
+
 // load items from storage
-$("#9 .textarea").val(localStorage.getItem("9"));
+$("#09 .textarea").val(localStorage.getItem("09"));
 $("#10 .textarea").val(localStorage.getItem("10"));
 $("#11 .textarea").val(localStorage.getItem("11"));
 $("#12 .textarea").val(localStorage.getItem("12"));
-$("#1 .textarea").val(localStorage.getItem("1"));
-$("#2 .textarea").val(localStorage.getItem("2"));
-$("#3 .textarea").val(localStorage.getItem("3"));
-$("#4 .textarea").val(localStorage.getItem("4"));
-$("#5 .textarea").val(localStorage.getItem("5"));
-$("#6 .textarea").val(localStorage.getItem("6"));
-// add the ability to save these blocks in the local storage
+$("#13 .textarea").val(localStorage.getItem("13"));
+$("#14 .textarea").val(localStorage.getItem("14"));
+$("#15 .textarea").val(localStorage.getItem("15"));
+$("#16 .textarea").val(localStorage.getItem("16"));
+$("#17 .textarea").val(localStorage.getItem("17"));
+$("#18 .textarea").val(localStorage.getItem("18"));
 
-
-//buttonEl.addEventListener("click", saveTasks);
-
-//  $(".container").on("click", "textarea", function() {
-//     // get the textarea's current value/text
-//     var text = $(this)
-//       .val()
-//       .trim();
-  
-//     // get the parent's id attribute
-//     var status = $(this)
-//       .closest(".container")
-//       .attr("id")
-//       .replace("text-1", "");
-  
-//     // get the task's position in the list of other div elements
-//     var index = $(this)
-//       .closest(".container")
-//       .index();
-//     tasks[status][index].text = text;
-//     saveTasks();
-  
-//     // recreate p element
-//     var taskP = $("<span>")
-//       .addClass("textarea col-8")
-//       .text(text);
-  
-//     // replace textarea with p element
-//     $(this).replaceWith(taskP);
-//     console.log('click');
-// });
-  
-// var loadTasks = function() {
-//     var savedTasks = localStorage.getItem("#save-me");
-  
-//     if (!savedTasks) {
-//       tasks = [];
-//       return false;
-//     }
-//     tasks = JSON.parse(localStorage.getItem("tasks"));
-//     savedTasks = JSON.parse(savedTasks);
-//     // if nothing in localStorage, create a new object to track all task status arrays
-//     if (!tasks) {
-//       tasks = {};
-//     }
-    
-//     // loop over object properties
-//     $.each(tasks, function(list, arr) {
-//        //then loop over sub-array
-//         arr.forEach(function(tasks) {
-//             createTask(tasks.text, list);
-//         });
-//     });
-// };
-// loadTasks();
-// // saveTasks();
-// // console.log(tasks)
-// // // make the blocks change color according to past present and future
+// make an interval to audit tasks 
+setInterval(function() {
+  timer();
+}, 35000);
